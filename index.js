c733bfc51c712976095bab838b6ec6c9f8c4f8d5
Eva Navarro <emanao@me.com>
2020-07-16 18:33:33 +0000

    createEmployeeRecord Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function createEmployeeRecord(data){
+  return {
+    firstName:data[0],
+    familyName:data[1],
+    title:data[2],
+    payPerHour:data[3],
+    timeInEvents:[],
+    timeOutEvents:[]
+  };
+}
